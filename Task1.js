4fb14c5d3608bc1e673ee5b9f49aa231779b061a
gamak grover <gamakgroverofficial@gmail.com>
2024-07-17 10:11:54 +0530

    changes

--- Task1.js
+++ Task1.js
@@ -32,6 +32,21 @@ const precedence = (c) => {
   else return -1;
 };
 // calculte the valur of converted array
+const calculate = (operand1, operand2, operator) => {
+  switch (operator) {
+    case "+":
+      sum = operand1 + operand2;
+      break;
+    case "-":
+      sum = operand1 - operand2;
+      break;
+    case "*":
+      sum = operand1 * operand2;
+      break;
+    case "/":
+      sum = operand1 / operand2;
+  }
+};
 const evaluate = (expression) => {
   let arr = toArray(expression);
   let operators = [];
@@ -42,23 +57,11 @@ const evaluate = (expression) => {
     } else {
       if (arr[i] == ")") {
         while (operators[operators.length - 1] != "(") {
-          let x = operands.pop();
-          let y = operands.pop();
-          let z = operators.pop();
-          let sum = 0;
-          switch (z) {
-            case "+":
-              sum = y + x;
-              break;
-            case "-":
-              sum = y - x;
-              break;
-            case "*":
-              sum = y * x;
-              break;
-            case "/":
-              sum = y / x;
-          }
+          let operand1 = operands.pop();
+          let operand2 = operands.pop();
+          let operator = operators.pop();
+          let sum = calculate(x, y, z);
+
           operands.push(sum);
         }
         operators.pop(); // Remove the '('
@@ -70,23 +73,11 @@ const evaluate = (expression) => {
           precedence(arr[i]) <= precedence(operators[operators.length - 1]) &&
           operators[operators.length - 1] != "("
         ) {
-          let x = operands.pop();
-          let y = operands.pop();
-          let z = operators.pop();
-          let sum = 0;
-          switch (z) {
-            case "+":
-              sum = y + x;
-              break;
-            case "-":
-              sum = y - x;
-              break;
-            case "*":
-              sum = y * x;
-              break;
-            case "/":
-              sum = y / x;
-          }
+          let operand1 = operands.pop();
+          let operand2 = operands.pop();
+          let operator = operators.pop();
+          let sum = calculate(operand1, operand2, operator);
+
           operands.push(sum);
         }
         operators.push(arr[i]);
@@ -94,53 +85,20 @@ const evaluate = (expression) => {
     }
   }
   while (operators.length > 0) {
-    let x = operands.pop();
-    let y = operands.pop();
-    let z = operators.pop();
-    let sum = 0;
-    switch (z) {
-      case "+":
-        sum = y + x;
-        break;
-      case "-":
-        sum = y - x;
-        break;
-      case "*":
-        sum = y * x;
-        break;
-      case "/":
-        sum = y / x;
-    }
+    let operand1 = operands.pop();
+    let operand2 = operands.pop();
+    let operator = operators.pop();
+    let sum = calculate(operand1, operand2, operator);
+
     operands.push(sum);
   }
-  console.log(operands[0]);
-};
-evaluate("//converts numbers to array and push
-const toArray = (s) => {
-  let stack = [];
-  for (let i = 0; i < s.length; ) {
-    let c = s.charAt(i);
-    if (
-      c === "+" ||
-      c === "-" ||
-      c === "*" ||
-      c === "/" ||
-      c === "(" ||
-      c === ")"
-    ) {
-      stack.push(c);
-      i++;
-    } else {
-      let num = 0;
-      while (i < s.length && !isNaN(parseInt(s.charAt(i)))) {
-        num = num * 10 + parseInt(s.charAt(i));
-        i++;
-      }
-      stack.push(num);
-    }
+  if (isNaN(operands[0])) {
+    console.log("Invalid Expression");
+  } else {
+    console.log(operands[0]);
   }
-  return stack;
-}
+};
+evaluate("2**2");
 // you need to focus on every edge case and try to handle as many edge cases as you can and try to throw user friendly error
 
-// you need to use better variable names 
+// you need to use better variable names
